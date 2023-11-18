c94373c4bccc1032d98e1da6478f835217974646
Dustin Wilson <dustin@dustinwilson.com>
2023-11-18 11:57:26 -0600

    Updated docopts

--- docopts.rb
+++ docopts.rb
@@ -12,15 +12,17 @@ class Docopts < Formula
       bin.install "docopts_darwin_amd64" => "docopts"
     end
   elsif Hardware::CPU.arm?
-    # https://github.com/docopt/docopts/archive/refs/tags/v0.6.4-with-no-mangle-double-dash.tar.gz
     url "https://github.com/docopt/docopts/archive/refs/tags/v#{version}.tar.gz"
     sha256 "5bf29a4eaa07cb3d1449077697d8746678cc490ee3e63cfe3e1025cebf2f4008"
 
     depends_on "go" => :build
 
     def install
+      system "go", "mod", "init", "docopts"
       system "go", "get", "github.com/docopt/docopt-go"
       system "go", "build", "docopts.go"
+      bin.install "docopts"
     end
   end
 end
+
