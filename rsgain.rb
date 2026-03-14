1634795e9fde8d4f0405301f89072dc59b29b189
Dustin Wilson <dustin@dustinwilson.com>
2026-03-14 10:32:19 -0500

    [rsgain] Initial commit

---
+++ rsgain.rb
@@ -0,0 +1,26 @@
+class Rsgain < Formula
+  desc "A simple, but powerful ReplayGain 2.0 tagging utility"
+  homepage "https://github.com/complexlogic/rsgain"
+  version "3.6"
+  url "https://github.com/complexlogic/rsgain/releases/download/v#{version}/rsgain-#{version}-source.tar.xz"
+  sha256 "392ad1407eea7737c6f63b6a0681a3df51f33033d2d16b644f407e6ef3f28013"
+  license "BSD-2-Clause"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "ffmpeg"
+  depends_on "libebur128"
+  depends_on "taglib"
+  depends_on "inih"
+  depends_on "fmt"
+
+  def install
+     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+     cd "build"
+     system "make", "install"
+  end
+
+  test do
+    assert_equal "rsgain #{version} - using:", shell_output("#{bin}/rsgain -v").strip
+  end
+end
