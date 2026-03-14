9e7d825ee559705d6cff19e5a633d86c22524f62
Dustin Wilson <dustin@dustinwilson.com>
2026-03-14 10:33:50 -0500

    [rsgain] Updated file hash

--- rsgain.rb
+++ rsgain.rb
@@ -3,7 +3,7 @@ class Rsgain < Formula
   homepage "https://github.com/complexlogic/rsgain"
   version "3.6"
   url "https://github.com/complexlogic/rsgain/releases/download/v#{version}/rsgain-#{version}-source.tar.xz"
-  sha256 "392ad1407eea7737c6f63b6a0681a3df51f33033d2d16b644f407e6ef3f28013"
+  sha256 "26d46f1240a83366e82cbc9121a467fc1dcc977c7adfb4e15c99ead6b3d07ec8"
   license "BSD-2-Clause"
 
   depends_on "cmake" => :build
